88a7829d6417e9ffde4d5f5976d19ffafbabd2ea
Jennifer Franco <jennyfranco31.jf@gmail.com>
2024-12-10 19:38:49 -0300

    destruir variables de session

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
-var pool = require ('./models/bd')
+var pool = require ('./models/bd');
+
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
-var loginRouter = require ('./routes/admin/login')
+var loginRouter = require ('./routes/admin/login');
 
-var carpasRouter = require('./routes/carpas')
-var eventosRouter = require('./routes/eventos')
-var promocionesRouter = require('./routes/promociones')
+var carpasRouter = require('./routes/carpas');
+var eventosRouter = require('./routes/eventos');
+var promocionesRouter = require('./routes/promociones');
+var adminRouter = require('./routes/admin/novedades');
 
 
 var app = express();
@@ -27,50 +30,51 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(session({
-//     secret: 'hsdhuybl1k3664jas', //20 caracteres como minimo
-//     resave: false,
-//     saveUninitialized: true
-//   }));
 
-//   app.get('/', function(req, res){
-//     var conocido = Boolean(req.session.nombre);
-  
-//     res.render('index', {
-//       title: 'Sesion en Express.js',
-//       conocido: conocido,
-//       nombre: req.session.nombre
-//     })
-//   });
-  
-//   app.post('/ingresar', function(req, res){
-//     if(req.body.nombre){
-//       req.session.nombre = req.body.nombre
-//     }
-//     res.redirect('/')
-//   });
-  
-//   // catch 404 and forward to error handler
-//   app.use(function(req, res, next) {
-//     next(createError(404));
-//   });
-  
-//   // error handler
-//   app.use(function(err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render('error');
-// });
+app.use(session({
+    secret: 'hsdhuybl1k3664jas', //20 caracteres como minimo
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  secured = async (req, res, next) => {
+    try {
+        console.log(req.session.id_usuario);
+        if (req.session.id_usuario){
+            next();
+        } else {
+            res.redirect('/admin/login')
+        }
+    } catch (error){
+        console.log(error);
+    }
+}
 
 app.use('/', indexRouter);
 app.use('/carpas', carpasRouter);
 app.use('/eventos', eventosRouter);
 app.use('/promociones', promocionesRouter);
 app.use('/admin/login', loginRouter);
+app.use ('/admin/novedades',secured, adminRouter);
+  
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+
 
 
 module.exports = app;
