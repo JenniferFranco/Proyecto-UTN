25f7966f0cfea8f6d6c80f320936f4000ffd8c8a
Jennifer Franco <jennyfranco31.jf@gmail.com>
2024-12-10 22:09:02 -0300

    m5u1

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get ('/', function (req, res, next){
+var novedadesModel = require('../../models/novedadesModel');
+
+router.get ('/',  async function (req, res, next){
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout:'admin/layout',
-        usuario: req.session.nombre
+        usuario: req.session.nombre,
+        novedades
     });
 });
+
+    //Elimnar un item de la lista
+    router.get('/elimnar/id', async (req, res, next) => {
+        const id = req.params.id;
+        await novedadesModel.deleteNovedadesById(id);
+        res.redirect('/admin/novedades')
+    });
+
 
 module.exports = router;
